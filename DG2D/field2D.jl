4f61c9905f40445ab98b7286db2950d12caaa435
Brandon Allen <ballen@mit.edu>
2019-08-22 17:47:59 -0400

    sketch of ChorinNS solver. introduce new auxiliary field struct and some nice abstractions, going to test with advectiona nd burgers eqn before running this

--- DG2D/field2D.jl
+++ DG2D/field2D.jl
@@ -1,6 +1,7 @@
 include("grid2D.jl")
 
 abstract type AbstractField2D end
+abstract type AbstractAuxiliaryField2D <: AbstractField2D end
 
 """
 Field2D(𝒢::Grid2D)
@@ -24,7 +25,6 @@ struct Field2D{T} <: AbstractField2D
     # field value and tendency
     ϕ::T
     ϕ̇::T
-    ϕ°::T
 
     # volume contributions to tendency
     𝚽::T
@@ -49,19 +49,117 @@ struct Field2D{T} <: AbstractField2D
     function Field2D(𝒢::Grid2D)
         ϕ  = zeros(𝒢.nGL)
         ϕ̇  = zeros(𝒢.nGL)
-        ϕ° = zeros(𝒢.nGL)
 
         𝚽  = zeros(𝒢.nGL)
+
         φˣ = zeros(𝒢.nGL)
         φʸ = zeros(𝒢.nGL)
 
         fˣ = zeros(𝒢.nGL)
         fʸ = zeros(𝒢.nGL)
+
         Δf = zeros(𝒢.nGL)
         ∮f = zeros(𝒢.nGL)
 
         r  = zeros(𝒢.nGL)
 
-        return new{typeof(ϕ)}(ϕ,ϕ̇,ϕ°, 𝚽, φˣ,φʸ, fˣ,fʸ, Δf,∮f, r)
+        return new{typeof(ϕ)}(ϕ,ϕ̇, 𝚽, φˣ,φʸ, fˣ,fʸ, Δf,∮f, r)
     end
+end
+
+"""
+AuxiliaryField2D(𝒢::Grid2D)
+
+# Description
+
+    Contains all the computational elements necessary to compute an auxiliary field
+
+# Arguments
+
+-   `mesh`: a mesh to compute on
+
+# Return Values:
+
+-   `u` : the field to be computed
+-   `u̇`: numerical solutions for the field
+-   `flux`: the numerical flux for the computation
+
+"""
+struct AuxiliaryField2D{T} <: AbstractAuxiliaryField2D
+    # physical value and numerical value
+    ϕ::T
+    ϕ°::T
+
+    # physical fluxes
+    φˣ::T
+    φʸ::T
+
+    # numerical fluxes
+    fˣ::T
+    fʸ::T
+
+    # jump in the flux
+    Δf::T
+
+    # surface contributions to the tendency
+    ∮f::T
+
+    function AuxiliaryField2D(𝒢::Grid2D)
+        ϕ  = zeros(𝒢.nGL)
+        ϕ° = zeros(𝒢.nGL)
+
+        φˣ = zeros(𝒢.nGL)
+        φʸ = zeros(𝒢.nGL)
+
+        fˣ = zeros(𝒢.nGL)
+        fʸ = zeros(𝒢.nGL)
+
+        Δf = zeros(𝒢.nGL)
+        ∮f = zeros(𝒢.nGL)
+
+        return new{typeof(ϕ)}(ϕ,ϕ°, φˣ,φʸ, fˣ,fʸ, Δf,∮f)
+    end
+end
+
+function computeCentralFluxes!(𝑓::Field2D, f::Face2D)
+    @. 𝑓.fˣ[f.i⁻] = 0.5 * (𝑓.φˣ[f.i⁻] + 𝑓.φˣ[f.i⁺])
+    @. 𝑓.fʸ[f.i⁻] = 0.5 * (𝑓.φʸ[f.i⁻] + 𝑓.φʸ[f.i⁺])
+
+    return nothing
+end
+
+function computeLaxFriedrichsFluxes!(𝑓::Field2D, f::Face2D)
+    C = maximum(abs.([𝑓.ϕ[f.i⁻]; 𝑓.ϕ[f.i⁺]]))
+    @. 𝑓.fˣ[f.i⁻] += C * f.nˣ * (𝑓.ϕ[f.i⁻] - 𝑓.ϕ[f.i⁺])
+    @. 𝑓.fʸ[f.i⁻] += C * f.nˣ * (𝑓.ϕ[f.i⁻] - 𝑓.ϕ[f.i⁺])
+
+    return nothing
+end
+
+function computeCentralDifference!(𝑓::AuxiliaryField2D, f::Face2D)
+    @. 𝑓.ϕ°[f.i⁻] = 0.5 * (𝑓.ϕ[f.i⁻] + 𝑓.ϕ[f.i⁺])
+
+    return nothing
+end
+
+function computeSurfaceTerms!(𝑓::Field2D, f::Face2D)
+    # compute jump in flux
+    @. 𝑓.Δf[f.i⁻] = f.nˣ * (𝑓.φˣ[f.i⁻] - 𝑓.fˣ[f.i⁻]) + f.nʸ * (𝑓.φʸ[f.i⁻] - 𝑓.fʸ[f.i⁻])
+
+    # compute surface terms
+    𝑓.∮f[f.iⱽ] = Ωᵏ.M⁺ * f.∮ * (f.C .* 𝑓.Δf[f.i⁻])
+    @. 𝑓.ϕ̇[f.iⱽ] += 𝑓.∮f[f.iⱽ]
+
+    return nothing
+end
+
+function computeSurfaceTerms!(𝑓::AuxiliaryField2D, f::Face2D)
+    # compute jump in flux
+    @. 𝑓.Δf[f.i⁻] = f.nˣ * (𝑓.φˣ[f.i⁻] - 𝑓.fˣ[f.i⁻]) + f.nʸ * (𝑓.φʸ[f.i⁻] - 𝑓.fʸ[f.i⁻])
+
+    # compute surface terms
+    𝑓.∮f[f.iⱽ] = Ωᵏ.M⁺ * f.∮ * (f.C .* 𝑓.Δf[f.i⁻])
+    @. 𝑓.ϕ[f.iⱽ] -= 𝑓.∮f[f.iⱽ]
+
+    return nothing
 end
---
+++ DG2D/solveChorinNS.jl
@@ -0,0 +1,149 @@
+include("field2D.jl")
+include("utils2D.jl")
+
+"""
+solveSalmonCNS!(fields, params)
+
+# Description
+
+    numerical solution to Chorin Navier Stokes equation
+    in vector form:
+    ∂ᵗũ = -∇(ũ⨂ũ) + c²∇(∇⋅ũ) + ν∇²ũ
+    written out component wise for DG formulation:
+    ∂ᵗu = -∂ˣ(uu - (ν+c²)uˣ - c²vʸ) - ∂ʸ(uv - νuʸ)
+    ∂ᵗv = -∂ʸ(vv - (ν+c²)vʸ - c²uˣ) - ∂ˣ(vu - νvˣ)
+
+
+# Arguments
+
+-   `fields = (u, v)`: velocity in each dimension
+-   `params = (𝒢, ν, c²)`: grid struct, viscosity, and speed of sound
+-   `BCᵈ = (Dᵘ, Dᵛ)`: dirichlet boundary conditions for each field
+-   `BCⁿ = (Nᵘ, Nᵛ)`:   neumann boundary conditions for each field
+
+"""
+function solveChorinNS!(fields, params, time; BCᵈ = [nothing, nothing, nothing], BCⁿ = [nothing, nothing, nothing])
+    # unpack parameters
+    𝒢  = params[1]
+    ν  = params[2]
+    c² = params[3]
+
+    # main velocity fields
+    u  = fields[1]
+    v  = fields[2]
+
+    # utility fields for first derivatives
+    uˣ = fields[3]
+    uʸ = fields[4]
+    vˣ = fields[5]
+    vʸ = fields[6]
+
+    # utility fields for second order terms
+    uu = fields[7]
+    uv = fields[8]
+    vu = fields[9]
+    vv = fields[10]
+
+    # for convenience
+    nonlinear   = [uu, uv, vu, vv]
+    derivatives = [uˣ, uʸ, vˣ, vʸ]
+    auxiliary   = nonlinear + derivatives
+
+    for Ωᵏ in 𝒢.Ω
+        # get volume nodes
+        iⱽ = Ωᵏ.iⱽ
+
+        # compute volume contributions to first derivatives
+        ∇!(u.φˣ, u.φʸ, u.ϕ, Ωᵏ)
+        @. uˣ.ϕ[iⱽ] = u.φˣ[iⱽ]
+        @. uʸ.ϕ[iⱽ] = u.φʸ[iⱽ]
+
+        ∇!(v.φˣ, v.φʸ, v.ϕ, Ωᵏ)
+        @. vˣ.ϕ[iⱽ] = v.φˣ[iⱽ]
+        @. vʸ.ϕ[iⱽ] = v.φʸ[iⱽ]
+
+        # define physical fluxes for first derivatives
+        @. uˣ.φˣ[iⱽ] = u.ϕ[iⱽ]
+        @. uʸ.φʸ[iⱽ] = u.ϕ[iⱽ]
+
+        @. vˣ.φˣ[iⱽ] = v.ϕ[iⱽ]
+        @. vʸ.φʸ[iⱽ] = v.ϕ[iⱽ]
+
+        # compute surface contributions to first derivatives
+        for f in Ωᵏ.faces
+            for 𝑓 in derivatives
+                computeCentralFluxes!(𝑓, f)
+            end
+
+            # impose BC
+            if f.isBoundary[1]
+                uᴮ = [u⁰(𝒢.x[i],t) for i in f.i⁻]
+                @. uˣ.fˣ[f.i⁻] = uᴮ
+                @. uʸ.fʸ[f.i⁻] = uᴮ
+
+                vᴮ = [v⁰(𝒢.x[i],t) for i in f.i⁻]
+                @. vˣ.fˣ[f.i⁻] = vᴮ
+                @. vʸ.fʸ[f.i⁻] = vᴮ
+            end
+
+            for 𝑓 in derivatives
+                computeSurfaceTerms!(𝑓, f)
+            end
+        end
+
+        # compute non-linear terms
+        @. uu.ϕ[iⱽ] = u.ϕ[iⱽ] * u.ϕ[iⱽ]
+        @. uv.ϕ[iⱽ] = u.ϕ[iⱽ] * v.ϕ[iⱽ]
+        @. vu.ϕ[iⱽ] = v.ϕ[iⱽ] * u.ϕ[iⱽ]
+        @. vv.ϕ[iⱽ] = v.ϕ[iⱽ] * v.ϕ[iⱽ]
+
+        # define physical fluxes for u and v
+        @. u.φˣ[iⱽ] = uu.ϕ[iⱽ] - (ν+c²) * uˣ.ϕ[iⱽ] - c² * vʸ.ϕ[iⱽ]
+        @. u.φʸ[iⱽ] = uv.ϕ[iⱽ] - ν * uʸ.ϕ[iⱽ]
+
+        @. v.φˣ[iⱽ] = vu.ϕ[iⱽ] - ν * vˣ.ϕ[iⱽ]
+        @. v.φʸ[iⱽ] = vv.ϕ[iⱽ] - (ν+c²) * vʸ.ϕ[iⱽ] - c² * uˣ.ϕ[iⱽ]
+
+        # compute volume contributions to the tendecies
+        ∇⨀!(u.𝚽, u.φˣ, u.φʸ, Ωᵏ)
+        @. u.ϕ̇[iⱽ] = -u.𝚽[iⱽ]
+
+        ∇⨀!(v.𝚽, v.φˣ, v.φʸ, Ωᵏ)
+        @. v.ϕ̇[iⱽ] = -v.𝚽[iⱽ]
+
+        # compute surface contributions to tendency
+        for f in Ωᵏ.faces
+            for 𝑓 in auxiliary
+                computeCentralDifference!(𝑓, f)
+            end
+
+            # impose BC on auxiliary fields
+            if f.isBoundary[1]
+                uᴮ = [u⁰(𝒢.x[i],t) for i in f.i⁻]
+                vᴮ = [v⁰(𝒢.x[i],t) for i in f.i⁻]
+                @. uu.ϕ°[f.i⁻] = uᴮ * uᴮ
+                @. uv.ϕ°[f.i⁻] = uᴮ * vᴮ
+                @. vu.ϕ°[f.i⁻] = vᴮ * uᴮ
+                @. vv.ϕ°[f.i⁻] = vᴮ * vᴮ
+
+                @. uˣ.ϕ°[f.i⁻] = uˣ.ϕ[f.i⁻]
+                @. uʸ.ϕ°[f.i⁻] = uʸ.ϕ[f.i⁻]
+                @. vˣ.ϕ°[f.i⁻] = vˣ.ϕ[f.i⁻]
+                @. vʸ.ϕ°[f.i⁻] = vʸ.ϕ[f.i⁻]
+            end
+
+            # compute numerical fluxes
+            @. u.fˣ[f.i⁻] = uu.ϕ°[f.i⁻] - (ν+c²) * uˣ.ϕ°[f.i⁻] - c² * vʸ.ϕ°[f.i⁻]
+            @. u.fʸ[f.i⁻] = uv.ϕ°[f.i⁻] - ν * uʸ.ϕ°[f.i⁻]
+            computeLaxFriedrichsFluxes!(u, f)
+            computeSurfaceTerms!(u, f)
+
+            @. v.fˣ[f.i⁻] = vu.ϕ°[f.i⁻] - ν * vˣ.ϕ°[f.i⁻]
+            @. v.fʸ[f.i⁻] = vv.ϕ°[f.i⁻] - (ν+c²) * vʸ.ϕ°[f.i⁻] - c² * uˣ.ϕ°[f.i⁻]
+            computeLaxFriedrichsFluxes!(v, f)
+            computeSurfaceTerms!(v, f)
+        end
+    end
+
+    return nothing
+end
